67f04a7056d1112c2821bfc41bfa2ce185f824ac
Lucio Pericoli <luciopericoli@gmail.com>
2021-11-24 18:42:36 -0300

    probando

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
-import CartWidgets from './Components/CartWidget.js/CartWidgets';
 import React ,{ Component } from 'react';
-import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
-import ItemCount from './Components/ItemCount/ItemCount';
+import Navbar from './Components/Navbar/Navbar';
+
+
 
 class App extends Component {
   constructor(props) {
@@ -19,42 +19,7 @@ class App extends Component {
 
   return (
     <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <ItemListContainer manito={this.state.greetings} />
-      </li>
-      <li class="nav-item">
-        <CartWidgets />
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="/">Action</a>
-          <a class="dropdown-item" href="/">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="/">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="/">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        </input>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+      <Navbar greetings={this.state.greetings}/>
     </div>
   );
 }
